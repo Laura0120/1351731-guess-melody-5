22242fcd0b28817368d6091e4d620d97acf3328a
a_galustyan <a_galustyan@head-point.ru>
2020-10-02 17:59:49 +0300

    маршрут по тайным тропам

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import WelcomeScreen from '../welcome-screen/welcome-screen';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import ArtistQuestionScreen from '../artist-question-screen/artist-question-screen';
+import GenreQuestionScreen from '../genre-question-screen/genre-question-screen';
+import AuthScreen from '../auth-screen/auth-screen';
+import GameOverScreen from '../game-over-screen/game-over-screen';
+import WinScreen from '../win-screen/win-screen';
 
 const App = (props) => {
   const {errorsCount} = props;
 
-  return <WelcomeScreen errorsCount={errorsCount} />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={WelcomeScreen} errorsCount={errorsCount} />
+        <Route exact path='/dev-artist' component={ArtistQuestionScreen} />
+        <Route exact path='/dev-genre' component={GenreQuestionScreen} />
+        <Route exact path='/login' component={AuthScreen} />
+        <Route exact path='/result' component={WinScreen} />
+        <Route exact path='/lose' component={GameOverScreen} />
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 App.propTypes = {
