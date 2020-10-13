ca9ff8f466b986dc5ca7ae6b3b702040ca366463
a_galustyan <a_galustyan@head-point.ru>
2020-10-13 20:48:10 +0300

    обратная сторона реальности

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -7,19 +7,38 @@ import GenreQuestionScreen from '../genre-question-screen/genre-question-screen'
 import AuthScreen from '../auth-screen/auth-screen';
 import GameOverScreen from '../game-over-screen/game-over-screen';
 import WinScreen from '../win-screen/win-screen';
+import GameScreen from '../game-screen/game-screen';
 
 const App = (props) => {
-  const {errorsCount} = props;
+  const {errorsCount, questions} = props;
+  const [firstQuestion, secondQuestion] = questions;
 
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path='/' component={WelcomeScreen} errorsCount={errorsCount} />
-        <Route exact path='/dev-artist' component={ArtistQuestionScreen} />
-        <Route exact path='/dev-genre' component={GenreQuestionScreen} />
-        <Route exact path='/login' component={AuthScreen} />
-        <Route exact path='/result' component={WinScreen} />
-        <Route exact path='/lose' component={GameOverScreen} />
+        <Route
+          exact
+          path='/'
+          render={({history}) => <WelcomeScreen onPlayButtonClick={() => history.push(`/game`)} errorsCount={errorsCount} />}
+        />
+        <Route exact path='/dev-artist'>
+          <ArtistQuestionScreen question={secondQuestion} onAnswer={() => {}} />
+        </Route>
+        <Route exact path='/dev-genre'>
+          <GenreQuestionScreen question={firstQuestion} onAnswer={() => {}} />
+        </Route>
+        <Route exact path='/login'>
+          <AuthScreen />
+        </Route>
+        <Route exact path='/result'>
+          <WinScreen />
+        </Route>
+        <Route exact path='/lose'>
+          <GameOverScreen />
+        </Route>
+        <Route exact path='/game'>
+          <GameScreen errorsCount={errorsCount} questions={questions} />
+        </Route>
       </Switch>
     </BrowserRouter>
   );
@@ -27,6 +46,7 @@ const App = (props) => {
 
 App.propTypes = {
   errorsCount: PropTypes.number.isRequired,
+  questions: PropTypes.array.isRequired,
 };
 
 export default App;
